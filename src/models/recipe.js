365f0b546a4709a04ac837e8f60cbc0be8bc331c
shreeup <shree912@yahoo.com>
2024-03-12 20:15:21 -0700

    fix build

--- src/models/recipe.js
+++ src/models/recipe.js
@@ -1,19 +1,12 @@
 const mongoose = require("mongoose");
-
-const RecipeSchema = new mongoose.Schema({
-  name: {
-    type: String,
-  },
-  preperation: {
-    type: String,
-  },
-  ingredients: {
-    type: [String],
-  },
-  dietarylabels: {
-    type: [String],
+const RecipeSchema = new mongoose.Schema(
+  {
+    name: String,
+    howtoprepare: String,
+    ingredients: [String],
+    dietlabels: [String],
   },
   { timestamps: true }
-});
+);
 
 module.exports = mongoose.model("Recipe", RecipeSchema);
--- src/models/user.js
+++ src/models/user.js
@@ -1,23 +1,52 @@
-const mongoose = require('mongoose');
-//const bcrypt = require('bcryptjs') add later
-
-
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const UserSchema = new mongoose.Schema({
-    email:{
-        type: String,
-        required:[true, 'Please provide email'],
-        match: [
-            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, 'Please provide valid email'
-        ],
-        unique: true
-    },
-
-    password:{
-        type: String,
-        required:[true, 'Please provide password'],
-        minlength: 6,   
-    }
+  name: {
+    type: String,
+    required: [true, "Please give name"],
+    minlength: 3,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    required: [true, "Please give email"],
+    match: [
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      ,
+      "Please give valid email",
+    ],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Please give password"],
+    minlength: 6,
+  },
 });
 
+UserSchema.pre("save", async function () {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
 
-module.exports = mongoose.model('User', UserSchema);
+UserSchema.methods.getName = function () {
+  return this.name;
+};
+
+UserSchema.methods.createJWT = async function () {
+  let tok = jwt.sign(
+    { userId: this._id, name: this.name },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: process.env.JWT_LIFETIME,
+    }
+  );
+  return tok;
+};
+
+UserSchema.methods.comparePassword = async function (candidatePassword) {
+  const isMatch = await bcrypt.compare(candidatePassword, this.password);
+  return isMatch;
+};
+module.exports = mongoose.model("User", UserSchema);
