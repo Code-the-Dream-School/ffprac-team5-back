abf8d955f255bc7e96d1ee6f6d99617c0e4699fb
shreeup <shree912@yahoo.com>
2024-03-12 20:25:57 -0700

    add file back

---
+++ src/models/recipe.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const RecipeSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+    },
+    preperation: {
+      type: String,
+    },
+    ingredients: {
+      type: [String],
+    },
+    dietarylabels: {
+      type: [String],
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Recipe", RecipeSchema);
