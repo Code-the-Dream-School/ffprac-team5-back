cd3750b41cd6261c7ce715b20b96994d2fdde761
lijjyt <124975092+lijjyt@users.noreply.github.com>
2024-03-21 15:23:43 -0700

    ingredient search specification

--- src/controllers/ingredientController.js
+++ src/controllers/ingredientController.js
@@ -4,13 +4,15 @@ const Ingredients = require("../models/ingredient");
 
 const getIngredients = async(req, res) => {
     try {
-        const ingredients = await Ingredients.find()
+        searchTerm = req.query.name
+        const ingredients = await Ingredients.find({ name: { $regex: searchTerm, $options: 'i' } });
         res.status(StatusCodes.OK).json(ingredients);
     } catch (error) {
-        console.log(e);
+        console.log(error);
         res.status(500).json({ msg: "Could not fetch ingredients" });
     }
 
 }
+
 
 module.exports = getIngredients;
--- src/models/ingredient.js
+++ src/models/ingredient.js
@@ -7,6 +7,9 @@ const ingredientSchema = new mongoose.Schema(
         number: {
             type: Number
         },
+        unit: {
+            type: String
+        },
     }   
 )
 
